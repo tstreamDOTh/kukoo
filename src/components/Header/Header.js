99d7712b7ab0c4449e05e4ea5e45671b651d1df3
T Thiyagaraj <tstream.h@outlook.com>
2018-05-19 23:57:33 +0530

    Add basic hamburger button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,17 +3,20 @@ import './Header.sass';
 import '../../style/layout.sass';
 
 class Header extends Component {
+
+
   render() {
+    var issueButton = <div className="hamburger"><div className="fabar" /><div className="fabar" /><div className="fabar" /></div>;
     return (
       <div className="Header flex">
-        <div className="flex-1 justify-start">
-          <p>Issue Boards </p>
+        <div className="flex-1 flex justify-start">
+          {issueButton}
         </div>
-        <div className="flex-1 justify-center">
-          <p>Issue Boards </p>
+        <div className="flex-1 flex justify-center">
+          <p>Logo </p>
         </div>
-        <div className="flex-1 justify-end">
-          <p>Issue Boards </p>
+        <div className="flex-1 flex justify-end">
+          <p>Settings </p>
         </div>
       </div>
     );
