2ec64181b47a1f8d41650ed67ead3877ef272761
T Thiyagaraj <tstream.h@outlook.com>
2018-05-26 23:00:37 +0530

    Remove default logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import Header from './components/Header/Header'
 import Board from './components/Board/Board'
 import BoardList from './components/BoardList/BoardList'
